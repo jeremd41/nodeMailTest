6ef5218ccbeb5bfa04692f902c317886e729e4e2
Jeremy <jeremy.diard37@gmail.com>
2020-08-04 21:13:14 +0200

    refactorisation

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const nodemailer = require("nodemailer")
 
-var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
@@ -14,8 +15,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+const contactAddress = "webizy.france@gmail.com"
+const mailer = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_ADRESS,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+})
+app.post("/contact", function (req, res) {
+  mailer.sendMail(
+    {
+      from: req.body.from,
+      to: [contactAddress],
+      subject: req.body.subject || "[No subject]",
+      html: req.body.message || "[No message]",
+    },
+    function (err, info) {
+      if (err) return res.status(500).send(err)
+      res.json({ success: true })
+    }
+  )
+})
 
 var listener = app.listen(8080, function() {
   console.log("Listening on port " + listener.address().port);
